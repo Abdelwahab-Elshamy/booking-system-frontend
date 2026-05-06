61fe9149e54d32a20b46d37bf189d5849c40d317
Abdelwahab <aa5070338@gmail.com>
2026-05-06 03:58:54 +0300

    updata api base url for production

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../store/store";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://booking-system-backend-production-5c29.up.railway.app/",
 });
 
 api.interceptors.request.use((config) => {
